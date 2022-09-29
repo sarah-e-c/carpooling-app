77f86e493decd55c738a6b6a23c611d99a8a8087
sarah-e-c <71732260+sarah-e-c@users.noreply.github.com>
2022-09-29 19:46:35 -0400

    oof

--- carpooling/__init__.py
+++ carpooling/__init__.py
@@ -1,12 +1,17 @@
 import flask_sqlalchemy
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
+import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://pbhbpribpxamgo:d7eb88d69d1f16ac2f1da19071c8e4c898c2fb48619d94be047f590003c4a847@ec2-18-209-78-11.compute-1.amazonaws.com:5432/d6dl85srl08ilp'
+
+database_url = os.environ.get('DATABASE_URL')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = database_url.replace('postgres://', 'postgresql://', 1)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
 db = SQLAlchemy(app)
+
 import carpooling.routes
 
